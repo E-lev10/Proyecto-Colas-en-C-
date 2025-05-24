11f20accfbf05b4f3e3781ec801f177f5212b0d3
E-lev10 <pasevelioyaxon40@gmail.com>
2025-05-23 21:48:14 -0600

    Seguimiento del codigo

--- Colas Proyecto.cpp
+++ Colas Proyecto.cpp
@@ -2,4 +2,11 @@
 #include <queue>  // Biblioteca para usar la estructura de datos 'cola' (FIFO)
 #include <string>  // Biblioteca para manejo de cadenas de texto (string)
 #include <windows.h> // Biblioteca específica de Windows para configuraciones de consola
-using namespace std;
+
+sing namespace std;      // Evita escribir 'std::' antes de cada función estándar
+
+// Estructura que define un cliente del banco
+struct Cliente {
+    int turno;           // Número de turno asignado al cliente
+    string nombre;       // Nombre del cliente (usamos string para permitir espacios)
+};
