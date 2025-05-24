2b6bb169628a126b724b41250796bbe45870a6c8
E-lev10 <pasevelioyaxon40@gmail.com>
2025-05-23 21:39:56 -0600

    agregue comentarios

--- Colas Proyecto.cpp
+++ Colas Proyecto.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
-#include <queue>
-#include <string>
-#include <windows.h> // Para SetConsoleOutputCP
+#include <iostream> // Biblioteca para operaciones de entrada/salida (cout, cin)
+#include <queue>  // Biblioteca para usar la estructura de datos 'cola' (FIFO)
+#include <string>  // Biblioteca para manejo de cadenas de texto (string)
+#include <windows.h> // Biblioteca específica de Windows para configuraciones de consola
 using namespace std;
